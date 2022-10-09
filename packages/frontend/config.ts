003202a29976ede04b4a8ccb4f40b7c5e8234565
jack <jhburrus3@gmail.com>
2022-10-09 08:09:38 -0500

    update head tag

--- packages/frontend/config.ts
+++ packages/frontend/config.ts
@@ -2,9 +2,9 @@
 // export const NETWORK_ID = 420 as number;
 
 // polygon-mumbai
-export const NETWORK_ID = 80001 as number;
+// export const NETWORK_ID = 80001 as number;
 
 //localhost
-// export const NETWORK_ID = 31337 as number;
+export const NETWORK_ID = 31337 as number;
 
 export const NETWORK_NAME = 'localhost' as string;
